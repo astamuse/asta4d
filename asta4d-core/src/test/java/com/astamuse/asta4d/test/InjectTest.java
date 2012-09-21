daeb66516fad6c698fad819f8f03432ccaa1efb7
e-ryu <e-ryu@c148c8b9-5c34-4071-8587-f0408d8675ae>
2012-09-21 04:06:51 +0000

    add test case for instance injection
    git-svn-id: http://svn.patent-office.jp/repos/develop/eureika_2013/project/trunk/eureika@13977 c148c8b9-5c34-4071-8587-f0408d8675ae

--- asta4d-core/src/test/java/com/astamuse/asta4d/test/InjectTest.java
+++ asta4d-core/src/test/java/com/astamuse/asta4d/test/InjectTest.java
@@ -2,6 +2,7 @@ package com.astamuse.asta4d.test;
 
 import com.astamuse.asta4d.Context;
 import com.astamuse.asta4d.data.annotation.ContextData;
+import com.astamuse.asta4d.render.GoThroughRenderer;
 import com.astamuse.asta4d.render.Renderer;
 import com.astamuse.asta4d.test.infra.BaseTest;
 import com.astamuse.asta4d.test.infra.SimpleCase;
@@ -37,6 +38,31 @@ public class InjectTest extends BaseTest {
         }
     }
 
+    public static class InstaneDefaultSearchRender {
+
+        @ContextData
+        private String cv;
+
+        private String gv;
+
+        public Renderer render() {
+            Renderer render = new GoThroughRenderer();
+            render.add("#cv-value", cv);
+            render.add("#gv-value", gv);
+            return render;
+        }
+
+        public String getGv() {
+            return gv;
+        }
+
+        @ContextData
+        public void setGv(String gv) {
+            this.gv = gv;
+        }
+
+    }
+
     public void testMethodDefaultSearch() {
         Context context = Context.getCurrentThreadContext();
         context.setData(Context.SCOPE_DEFAULT, "pv", "pv-value at context");
@@ -64,5 +90,33 @@ public class InjectTest extends BaseTest {
         context.setData(Context.SCOPE_GLOBAL, "gv-r", "gv-value for name replace");
         context.setData(Context.SCOPE_GLOBAL, "gv", "gv-value");
         new SimpleCase("Inject_testMethodNameSearch.html");
+    }
+
+    public void testInstanceDefaultSearch() {
+        Context context = Context.getCurrentThreadContext();
+        context.setData(Context.SCOPE_DEFAULT, "cv", "cv-value at context");
+        context.setData(Context.SCOPE_GLOBAL, "cv", "cv-value at global");
+        context.setData(Context.SCOPE_GLOBAL, "gv", "gv-value");
+        new SimpleCase("Inject_testInstanceDefaultSearch.html");
+    }
+
+    public void testInstanceScopeSearch() {
+        Context context = Context.getCurrentThreadContext();
+        context.setData(Context.SCOPE_DEFAULT, "av", "av-value at context");
+        context.setData(Context.SCOPE_DEFAULT, "pv", "pv-value at context");
+        context.setData(Context.SCOPE_DEFAULT, "cv", "cv-value at context");
+        context.setData(Context.SCOPE_GLOBAL, "cv", "cv-value at global");
+        context.setData(Context.SCOPE_DEFAULT, "gv", "gv-value at context");
+        context.setData(Context.SCOPE_GLOBAL, "gv", "gv-value at global");
+        // new SimpleCase("Inject_testMethodScopeSearch.html");
+    }
+
+    public void testInstanceNameSearch() {
+        Context context = Context.getCurrentThreadContext();
+        context.setData(Context.SCOPE_DEFAULT, "cv", "cv-value");
+        context.setData(Context.SCOPE_DEFAULT, "cv-r", "cv-value for name replace");
+        context.setData(Context.SCOPE_GLOBAL, "gv-r", "gv-value for name replace");
+        context.setData(Context.SCOPE_GLOBAL, "gv", "gv-value");
+        // new SimpleCase("Inject_testMethodNameSearch.html");
     }
 }
