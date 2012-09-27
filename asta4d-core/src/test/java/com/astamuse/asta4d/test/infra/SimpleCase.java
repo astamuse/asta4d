2a1d0c1b5d63c1a1a3a0f066519917fe92673f5f
s.otani <s.otani@c148c8b9-5c34-4071-8587-f0408d8675ae>
2012-09-27 02:15:44 +0000

    テストケースで比較用のファイルをjsoupでパースせずにそのまま取得するように変更
    git-svn-id: http://svn.patent-office.jp/repos/develop/eureika_2013/project/trunk/eureika@14057 c148c8b9-5c34-4071-8587-f0408d8675ae

--- asta4d-core/src/test/java/com/astamuse/asta4d/test/infra/SimpleCase.java
+++ asta4d-core/src/test/java/com/astamuse/asta4d/test/infra/SimpleCase.java
@@ -1,10 +1,9 @@
 package com.astamuse.asta4d.test.infra;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.testng.Assert;
 
 import com.astamuse.asta4d.Page;
@@ -34,15 +33,18 @@ public class SimpleCase {
     }
 
     private String revert2comparableString(Page page) {
-        String s = page.output();
-        Document doc = Jsoup.parse(s);
-        return doc.outerHtml();
+        return page.output();
     }
 
     private String revert2comparableString(String path) throws IOException {
-        try (InputStream in = SimpleCase.class.getResourceAsStream(path)) {
-            Document doc = Jsoup.parse(in, "utf-8", "");
-            return doc.outerHtml();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(SimpleCase.class.getResourceAsStream(path), "UTF-8"))) {
+            StringBuilder sb = new StringBuilder();
+            char[] b = new char[1024];
+            int line;
+            while (0 <= (line = reader.read(b))) {
+                sb.append(b, 0, line);
+            }
+            return sb.toString();
         }
     }
 
