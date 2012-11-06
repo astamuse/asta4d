0ebf7070bfa68054323e327fcfdef990d8843296
e-ryu <e-ryu@c148c8b9-5c34-4071-8587-f0408d8675ae>
2012-11-06 06:49:19 +0000

    add JPA runtime wave to jvm option
    git-svn-id: http://svn.patent-office.jp/repos/develop/eureika_2013/project/trunk/eureika@14362 c148c8b9-5c34-4071-8587-f0408d8675ae

--- startJetty.bat
+++ startJetty.bat
@@ -1,1 +1,3 @@
-mvn %1 test -DskipTests=true -Deureika.debug=true -Pjetty-run
+set topfolder=%~dp0
+set MAVEN_OPTS=-javaagent:"%topfolder%\eureika-web\libs\spring-instrument-3.1.2.RELEASE.jar" %MAVEN_OPTS%
+mvn %1 test -DskipTests=true -Deureika.debug=true -Pjetty-run 
