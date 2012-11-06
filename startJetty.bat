93ba5a02beb1a5e2332d973c43b5c5fc1b69c888
s.otani <s.otani@c148c8b9-5c34-4071-8587-f0408d8675ae>
2012-11-06 10:40:39 +0000

    IssueID #139 batファイルでデフォルトのシステムプロパティを指定
    git-svn-id: http://svn.patent-office.jp/repos/develop/eureika_2013/project/trunk/eureika@14380 c148c8b9-5c34-4071-8587-f0408d8675ae

--- startJetty.bat
+++ startJetty.bat
@@ -1,3 +1,3 @@
 set topfolder=%~dp0
 set MAVEN_OPTS=-javaagent:"%topfolder%\eureika-web\libs\spring-instrument-3.1.2.RELEASE.jar" %MAVEN_OPTS%
-mvn %1 test -DskipTests=true -Deureika.debug=true -Pjetty-run 
+mvn %1 test -DskipTests=true -Deureika.debug=true -Pjetty-run -Deureika.home="%topfolder%\eureika-web\workhome"
