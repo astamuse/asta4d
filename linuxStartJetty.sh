382155fe64fce450f910cc49ce026fe87efaba38
h.iwata <h.iwata@c148c8b9-5c34-4071-8587-f0408d8675ae>
2012-11-14 06:35:57 +0000

    IssueID #164 Linux版mvn起動ファイルを追加
    git-svn-id: http://svn.patent-office.jp/repos/develop/eureika_2013/project/trunk/eureika@14571 c148c8b9-5c34-4071-8587-f0408d8675ae

--- linuxStartJetty.sh
+++ linuxStartJetty.sh
@@ -4,6 +4,7 @@ set -e
 
 # Base directory for this entire project
 BASEDIR=$(cd $(dirname $0) && pwd)
+export MAVEN_OPTS=-javaagent:"$BASEDIR/eureika-web/libs/spring-instrument-3.1.2.RELEASE.jar" 
 
 mvn3 test -DskipTests=true -Deureika.debug=true -Pjetty-run -Deureika.home="$BASEDIR/eureika-web/workhome"
 
