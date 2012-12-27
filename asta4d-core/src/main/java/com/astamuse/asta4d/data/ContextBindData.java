b3860e0eff5483b4ab5550617a6d4c53c8b36337
e-ryu <e-ryu@c148c8b9-5c34-4071-8587-f0408d8675ae>
2012-12-27 10:00:00 +0000

    git-svn-id: http://svn.patent-office.jp/repos/develop/eureika_2013/project/trunk/eureika@16521 c148c8b9-5c34-4071-8587-f0408d8675ae

--- asta4d-core/src/main/java/com/astamuse/asta4d/data/ContextBindData.java
+++ asta4d-core/src/main/java/com/astamuse/asta4d/data/ContextBindData.java
@@ -17,42 +17,106 @@
 
 package com.astamuse.asta4d.data;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import com.astamuse.asta4d.Context;
 
-public class ContextBindData<R> {
+/**
+ * a ContextBindData is supposed to be cache in the current thread Context and
+ * should only be initialized once
+ * 
+ * @author e-ryu
+ * 
+ * @param <T>
+ */
+public abstract class ContextBindData<T> {
 
-    public static interface DataProvider<R> {
-        public R get();
+    private final static String MapKey = "##MapKey##" + ContextBindData.class.getName();
+
+    private static class DataWithLock {
+        volatile Object data = null;
+        volatile boolean valid = false;
+        // final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
     }
 
-    private final static AtomicInteger sequencer = new AtomicInteger();
+    private String bindKey = this.getClass().getName() + "##bind-key##23@23@@23";
 
-    private String bindKey = "bindKey-" + sequencer.incrementAndGet() + "##" + ContextBindData.class.getName();
+    private boolean contextSynchronizable;
 
-    // private T THIS = null;
-
-    private DataProvider<R> provider;
-
-    private ContextBindData() {
-        // TODO Auto-generated constructor stub
+    /**
+     * The default constructor is for convenience of lazy load data
+     */
+    public ContextBindData() {
+        this(false);
     }
 
-    public R get() {
-        Context context = Context.getCurrentThreadContext();
-        R r = context.getData(bindKey);
-        if (r == null) {
-            r = provider.get();
-            context.setData(bindKey, r);
+    /**
+     * If contextSynchronizable is set to false, no synchronization operations
+     * will be performed and it is just a convenience for lazy load data.
+     * 
+     * Otherwise, if contextSynchronizable is set to true, a Context based lock
+     * will be performed and the data will be load only once in the current
+     * context environment.
+     * 
+     * @param doSynchronize
+     */
+    public ContextBindData(boolean contextSynchronizable) {
+        this.contextSynchronizable = contextSynchronizable;
+        if (contextSynchronizable) {
+            Context context = Context.getCurrentThreadContext();
+            ConcurrentMap<String, Object> map = context.getData(MapKey);
+            Object obj = map.get(bindKey);
+            if (obj == null) {
+                obj = new DataWithLock();
+                obj = map.putIfAbsent(bindKey, obj);
+            }
         }
-        return r;
     }
 
-    public final static <R> ContextBindData<R> create(DataProvider<R> provider) {
-        ContextBindData<R> data = new ContextBindData<>();
-        // data.THIS = THIS;
-        data.provider = provider;
-        return data;
+    public final static void initConext(Context context) {
+        ConcurrentMap<String, Object> map = new ConcurrentHashMap<>();
+        context.setData(MapKey, map);
     }
+
+    public T get() {
+        if (contextSynchronizable) {
+            return getDataSynchronously();
+        } else {
+            Context context = Context.getCurrentThreadContext();
+            T data = context.getData(bindKey);
+            if (data == null) {
+                data = buildData();
+                context.setData(bindKey, data);
+            }
+            return data;
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private T getDataSynchronously() {
+        Context context = Context.getCurrentThreadContext();
+        ConcurrentMap<String, Object> map = context.getData(MapKey);
+
+        // map will not be null since we have initialized it in constructor
+        DataWithLock dl = (DataWithLock) map.get(bindKey);
+        if (!dl.valid) {
+            Object data;
+            // TODO I want to read jvm code...
+            synchronized (dl) {
+                if (!dl.valid) {
+                    data = dl.data;
+                    if (data == null) {
+                        data = buildData();
+                        dl.data = data;
+                    }
+                    dl.valid = true;
+                }
+            }
+        }
+        return (T) dl.data;
+    }
+
+    protected abstract T buildData();
 }
