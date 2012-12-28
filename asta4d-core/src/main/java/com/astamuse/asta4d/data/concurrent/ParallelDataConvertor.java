62fc344417b0e891fce911f82bda8006dbc0db11
e-ryu <e-ryu@c148c8b9-5c34-4071-8587-f0408d8675ae>
2012-12-28 03:28:20 +0000

    ParallelDataConvertor should copy Context as well
    git-svn-id: http://svn.patent-office.jp/repos/develop/eureika_2013/project/trunk/eureika@16638 c148c8b9-5c34-4071-8587-f0408d8675ae

--- asta4d-core/src/main/java/com/astamuse/asta4d/data/concurrent/ParallelDataConvertor.java
+++ asta4d-core/src/main/java/com/astamuse/asta4d/data/concurrent/ParallelDataConvertor.java
@@ -21,15 +21,22 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 
+import com.astamuse.asta4d.Context;
 import com.astamuse.asta4d.data.DataConvertor;
 
 public abstract class ParallelDataConvertor<S, T> implements DataConvertor<S, T> {
 
     public Future<T> invoke(ExecutorService es, final S data) {
+        final Context context = Context.getCurrentThreadContext();
         return es.submit(new Callable<T>() {
             @Override
             public T call() throws Exception {
-                return convert(data);
+                return Context.with(context.clone(), new Callable<T>() {
+                    @Override
+                    public T call() throws Exception {
+                        return convert(data);
+                    }
+                });
             }
         });
     }
