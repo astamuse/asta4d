3b415379dec09c50a81d17961551558dd8119a92
s.otani <s.otani@c148c8b9-5c34-4071-8587-f0408d8675ae>
2013-03-01 03:27:50 +0000

    Linux 用 AstaCareer 起動ファイル
    git-svn-id: http://svn.patent-office.jp/repos/develop/eureika_2013/project/trunk/eureika@20896 c148c8b9-5c34-4071-8587-f0408d8675ae

--- linuxStartJetty.sh
+++ linuxStartJetty.sh
@@ -6,5 +6,5 @@ set -e
 BASEDIR=$(cd $(dirname $0) && pwd)
 export MAVEN_OPTS=-javaagent:"$BASEDIR/eureika-web/libs/spring-instrument-3.1.2.RELEASE.jar" 
 
-mvn3 test -DskipTests=true -Deureika.debug=true -Pjetty-run -Deureika.home="$BASEDIR/eureika-web/workhome"
+mvn3 test -DskipTests=true -Deureika.debug=true -Peureika-jetty-run -Deureika.home="$BASEDIR/eureika-web/workhome"
 
---
+++ linuxStartJetty4AstaCareer.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Base directory for this entire project
+BASEDIR=$(cd $(dirname $0) && pwd)
+export MAVEN_OPTS=-javaagent:"$BASEDIR/asta-career/libs/spring-instrument-3.1.2.RELEASE.jar" 
+
+mvn3 test -DskipTests=true -Deureika.debug=true -Pasta-career-jetty-run -Djetty.port=8090 -Deureika.home="$BASEDIR/asta-career/workhome"
+
