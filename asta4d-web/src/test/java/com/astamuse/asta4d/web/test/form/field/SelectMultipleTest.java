89ca6e4939525f096a861112dadf54a860282061
e-ryu <e.ryu@astamuse.co.jp>
2014-10-02 16:53:14 +0900

    add tests for multiple select

--- asta4d-web/src/test/java/com/astamuse/asta4d/web/test/form/field/SelectMultipleTest.java
+++ asta4d-web/src/test/java/com/astamuse/asta4d/web/test/form/field/SelectMultipleTest.java
@@ -64,6 +64,44 @@ public class SelectMultipleTest extends WebTestBase {
             return builder.toRenderer(true);
         }
 
+        public Renderer normalEditMultiValue() {
+            FieldRenderBuilder builder = FieldRenderBuilder.of(SelectMultipleRenderer.class);
+
+            builder.addPrepare(new SelectPrepareRenderer("nullvalue").setOptionData(createMap("x", "y", "z")));
+            builder.addValue("nullvalue", new Object[] { "a", null });
+
+            builder.addPrepare(new SelectPrepareRenderer("nullvalue-2").setOptionData(createMap("x", "y", "z", "")));
+            builder.addValue("nullvalue-2", new Object[] { "x", null });
+
+            builder.addPrepare(new SelectPrepareRenderer("emptyvalue").setOptionData(createMap("x", "y", "z")));
+            builder.addValue("emptyvalue", new Object[] { "x", "" });
+
+            builder.addPrepare(new SelectPrepareRenderer("emptyvalue-2").setOptionData(createMap("x", "y", "z", "")));
+            builder.addValue("emptyvalue-2", new Object[] { "a", "" });
+
+            builder.addPrepare(new SelectPrepareRenderer("spacevalue").setOptionData(createMap("x", "y", "z")));
+            builder.addValue("spacevalue", new Object[] { "a", " " });
+
+            builder.addPrepare(new SelectPrepareRenderer("spacevalue-2").setOptionData(createMap("x", "y", "z", " ")));
+            builder.addValue("spacevalue-2", new Object[] { "a", " " });
+
+            builder.addPrepare(new SelectPrepareRenderer("xvalue").setOptionData(createMap("y", "z")));
+            builder.addValue("xvalue", new Object[] { "x", "a" });
+
+            builder.addPrepare(new SelectPrepareRenderer("xvalue-2").setOptionData(createMap("x", "y", "z", "")));
+            builder.addValue("xvalue-2", new Object[] { "x", "y", "a" });
+
+            builder.addPrepare(new SelectPrepareRenderer("yvalue").addOptionGroup("A", createMap("x", "z")).addOptionGroup("AA",
+                    createMap("xx", "zz")));
+            builder.addValue("yvalue", new Object[] { "y", "zz" });
+
+            builder.addPrepare(new SelectPrepareRenderer("yvalue-2").addOptionGroup("A", createMap("x", "y", "z")).addOptionGroup("AA",
+                    createMap("xx", "yy", "zz")));
+            builder.addValue("yvalue-2", new Object[] { "y", "yy" });
+
+            return builder.toRenderer(true);
+        }
+
         public Renderer normalDisplayValue() {
             FieldRenderBuilder builder = FieldRenderBuilder.of(SelectMultipleRenderer.class);
 
@@ -110,6 +148,52 @@ public class SelectMultipleTest extends WebTestBase {
             return builder.toRenderer(false);
         }
 
+        public Renderer normalDisplayMultiValue() {
+            FieldRenderBuilder builder = FieldRenderBuilder.of(SelectMultipleRenderer.class);
+
+            builder.addPrepare(new SelectPrepareRenderer("nullvalue").setOptionData(createMap("x", "y", "z")));
+            builder.addValue("nullvalue", new Object[] { "a", null });
+
+            builder.addPrepare(new SelectPrepareRenderer("nullvalue-2").setOptionData(createMap("x", "y", "z", "")));
+            builder.addValue("nullvalue-2", new Object[] { "x", null });
+
+            builder.addPrepare(new SelectPrepareRenderer("emptyvalue").setOptionData(createMap("x", "y", "z")));
+            builder.addValue("emptyvalue", new Object[] { "a", "" });
+
+            builder.addPrepare(new SelectPrepareRenderer("emptyvalue-2").setOptionData(createMap("x", "y", "z", "")));
+            builder.addValue("emptyvalue-2", new Object[] { "y", "" });
+
+            builder.addPrepare(new SelectPrepareRenderer("spacevalue").setOptionData(createMap("x", "y", "z")));
+            builder.addValue("spacevalue", new Object[] { "a", " " });
+
+            builder.addPrepare(new SelectPrepareRenderer("spacevalue-2").setOptionData(createMap("x", "y", "z", " ")));
+            builder.addValue("spacevalue-2", new Object[] { "z", " " });
+
+            builder.addPrepare(new SelectPrepareRenderer("xvalue").setOptionData(createMap("y", "z")));
+            builder.addValue("xvalue", new Object[] { "x", "" });
+
+            builder.addPrepare(new SelectPrepareRenderer("xvalue-2").setOptionData(createMap("x", "y", "z", "")));
+            builder.addValue("xvalue-2", new Object[] { "x", "z" });
+
+            builder.addPrepare(new SelectPrepareRenderer("yvalue").addOptionGroup("A", createMap("x", "z")).addOptionGroup("AA",
+                    createMap("xx", "zz")));
+            builder.addValue("yvalue", new Object[] { "y", "a" });
+
+            builder.addPrepare(new SelectPrepareRenderer("yvalue-2").addOptionGroup("A", createMap("x", "y", "z")).addOptionGroup("AA",
+                    createMap("xx", "yy", "zz")));
+            builder.addValue("yvalue-2", new Object[] { "y", "zz" });
+
+            builder.addPrepare(new SelectPrepareRenderer("zvalue").addOptionGroup("A", createMap("x", "y")).addOptionGroup("AA",
+                    createMap("xx", "yy")));
+            builder.addValue("zvalue", new Object[] { "a", "z" });
+
+            builder.addPrepare(new SelectPrepareRenderer("zvalue-2").addOptionGroup("A", createMap("x", "y", "z")).addOptionGroup("AA",
+                    createMap("xx", "yy", "zz")));
+            builder.addValue("zvalue-2", new Object[] { "z", "zz", "a" });
+
+            return builder.toRenderer(false);
+        }
+
         public Renderer staticOptionEditValue() {
             FieldRenderBuilder builder = FieldRenderBuilder.of(SelectMultipleRenderer.class);
 
@@ -132,6 +216,32 @@ public class SelectMultipleTest extends WebTestBase {
             builder.addValue("yvalue", "y");
 
             builder.addValue("yvalue-2", "y");
+
+            return builder.toRenderer(true);
+        }
+
+        public Renderer staticOptionEditMultiValue() {
+            FieldRenderBuilder builder = FieldRenderBuilder.of(SelectMultipleRenderer.class);
+
+            builder.addValue("nullvalue", new Object[] { "a", null });
+
+            builder.addValue("nullvalue-2", new Object[] { "z", null });
+
+            builder.addValue("emptyvalue", new Object[] { "a", "" });
+
+            builder.addValue("emptyvalue-2", new Object[] { "a", "" });
+
+            builder.addValue("spacevalue", new Object[] { "a", " " });
+
+            builder.addValue("spacevalue-2", new Object[] { "z", " " });
+
+            builder.addValue("xvalue", new Object[] { "x", "a" });
+
+            builder.addValue("xvalue-2", new Object[] { "x", "a" });
+
+            builder.addValue("yvalue", new Object[] { "y", "a" });
+
+            builder.addValue("yvalue-2", new Object[] { "y", "z" });
 
             return builder.toRenderer(true);
         }
@@ -161,6 +271,31 @@ public class SelectMultipleTest extends WebTestBase {
 
             return builder.toRenderer(false);
         }
+
+        public Renderer staticOptionDisplayMultiValue() {
+            FieldRenderBuilder builder = FieldRenderBuilder.of(SelectMultipleRenderer.class);
+            builder.addValue("nullvalue", new Object[] { "a", null });
+
+            builder.addValue("nullvalue-2", new Object[] { "z", null });
+
+            builder.addValue("emptyvalue", new Object[] { "a", "" });
+
+            builder.addValue("emptyvalue-2", new Object[] { "a", "" });
+
+            builder.addValue("spacevalue", new Object[] { "a", " " });
+
+            builder.addValue("spacevalue-2", new Object[] { "z", " " });
+
+            builder.addValue("xvalue", new Object[] { "x", "a" });
+
+            builder.addValue("xvalue-2", new Object[] { "x", "a" });
+
+            builder.addValue("yvalue", new Object[] { "y", "a" });
+
+            builder.addValue("yvalue-2", new Object[] { "y", "z" });
+
+            return builder.toRenderer(false);
+        }
     }
 
     @SuppressWarnings("deprecation")
@@ -179,15 +314,31 @@ public class SelectMultipleTest extends WebTestBase {
         new FormRenderCase("/SelectMultiple_normalEdit.html");
     }
 
+    public void testNormalEditMultiValue() {
+        new FormRenderCase("/SelectMultiple_normalEditMultiValue.html");
+    }
+
     public void testNormalDisplay() {
         new FormRenderCase("/SelectMultiple_normalDisplay.html");
+    }
+
+    public void testNormalDisplayMultiValue() {
+        new FormRenderCase("/SelectMultiple_normalDisplayMultiValue.html");
     }
 
     public void testStaticOptionEdit() {
         new FormRenderCase("/SelectMultiple_staticOptionEdit.html");
     }
 
+    public void testStaticOptionEditMultiValue() {
+        new FormRenderCase("/SelectMultiple_staticOptionEditMultiValue.html");
+    }
+
     public void testStaticOptionDisplay() {
         new FormRenderCase("/SelectMultiple_staticOptionDisplay.html");
+    }
+
+    public void testStaticOptionDisplayMultiValue() {
+        new FormRenderCase("/SelectMultiple_staticOptionDisplayMultiValue.html");
     }
 }
