c240a5f60a49e70bea856c3a0812dda11a9efd93
e-ryu <e.ryu@astamuse.co.jp>
2014-10-21 18:41:33 +0900

    make sure the snippet instance will be initialized only once(for rare cases that cannot be warranted in previous implementation)

--- asta4d-core/src/main/java/com/astamuse/asta4d/snippet/interceptor/ContextDataAutowireInterceptor.java
+++ asta4d-core/src/main/java/com/astamuse/asta4d/snippet/interceptor/ContextDataAutowireInterceptor.java
@@ -17,7 +17,6 @@
 
 package com.astamuse.asta4d.snippet.interceptor;
 
-import com.astamuse.asta4d.Context;
 import com.astamuse.asta4d.data.DataOperationException;
 import com.astamuse.asta4d.data.InjectUtil;
 import com.astamuse.asta4d.interceptor.base.ExceptionHandler;
@@ -26,24 +25,10 @@ import com.astamuse.asta4d.snippet.SnippetInvokeException;
 
 public class ContextDataAutowireInterceptor implements SnippetInterceptor {
 
-    private final static String InstanceCacheKey = ContextDataAutowireInterceptor.class + "##InstanceCacheKey##";
-
     @Override
     public boolean beforeProcess(SnippetExecutionHolder execution) throws Exception {
 
         try {
-            Context context = Context.getCurrentThreadContext();
-            Object lastSnippetInstance = context.getData(InstanceCacheKey);
-            // it means that we need an instance injection and a reverse
-            // injection
-            if (lastSnippetInstance != execution.getInstance()) {
-                if (lastSnippetInstance != null) {
-                    InjectUtil.setContextDataFromInstance(lastSnippetInstance);
-                }
-                InjectUtil.injectToInstance(execution.getInstance());
-                context.setData(InstanceCacheKey, execution.getInstance());
-            }
-
             Object[] params = InjectUtil.getMethodInjectParams(execution.getMethod());
             execution.setParams(params);
             return true;
