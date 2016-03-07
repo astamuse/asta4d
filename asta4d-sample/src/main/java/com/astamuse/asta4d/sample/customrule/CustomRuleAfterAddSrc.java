23cdb37894a93359f9de29df4319a3bf2fc1dab7
e-ryu <e.ryu@astamuse.co.jp>
2016-03-07 15:33:22 +0900

    fix compile issue by oracle jdk due to the bad type inference ability...

--- asta4d-sample/src/main/java/com/astamuse/asta4d/sample/customrule/CustomRuleAfterAddSrc.java
+++ asta4d-sample/src/main/java/com/astamuse/asta4d/sample/customrule/CustomRuleAfterAddSrc.java
@@ -6,7 +6,7 @@ import com.astamuse.asta4d.web.dispatch.mapping.handy.HandyRuleAfterAttr;
 import com.astamuse.asta4d.web.dispatch.mapping.handy.HandyRuleAfterHandler;
 
 @SuppressWarnings({ "rawtypes", "unchecked" })
-public class CustomRuleAfterAddSrc extends HandyRuleAfterAddSrc<CustomRuleAfterAddSrc, HandyRuleAfterAttr, HandyRuleAfterHandler>
+public class CustomRuleAfterAddSrc extends HandyRuleAfterAddSrc<CustomRuleAfterAddSrc, HandyRuleAfterAttr<?, ?>, HandyRuleAfterHandler<?>>
         implements CustomRuleGroupConfigurable<CustomRuleAfterAddSrc>, CustomRuleBuilder {
 
     public CustomRuleAfterAddSrc(UrlMappingRule rule) {
